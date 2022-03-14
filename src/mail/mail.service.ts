89fedd2f3a0785953744b3ff3cf04893ff5b0bcf
MIN <rudgh2879@naver.com>
2022-03-14 22:01:45 +0900

    6.8 Beautiful Emails

--- src/mail/mail.service.ts
+++ src/mail/mail.service.ts
@@ -9,15 +9,17 @@ export class MailService {
   constructor(
     @Inject(CONFIG_OPTIONS) private readonly options: MailModuleOptions,
   ) {
-    this.sendEmail('testing', 'test');
+    this.sendEmail('testing', 'kh-min7');
   }
 
-  private async sendEmail(subject: string, content: string) {
+  private async sendEmail(subject: string, template: string) {
     const form = new FormData();
     form.append('from', `Excited User <mailgun@${this.options.domain}>`);
     form.append('to', `rudgh2879@naver.com`);
     form.append('subject', subject);
-    form.append('text', content);
+    form.append('template', template);
+    form.append('v:username', 'min7!!!');
+    form.append('v:code', 'asasa');
     const response = await got(
       `https://api.mailgun.net/v3/${this.options.domain}/messages`,
       {
