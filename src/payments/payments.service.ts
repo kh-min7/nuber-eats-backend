5d49d5ae0b0ae99b20c5e43e4985fa07baa5c121
MIN <rudgh2879@naver.com>
2022-04-15 21:59:29 +0900

    stop

--- src/payments/payments.service.ts
+++ src/payments/payments.service.ts
@@ -75,7 +75,7 @@ export class PaymentService {
     }
   }
 
-  @Interval(2000)
+  // @Interval(2000)
   async checkPromotedRestaurants() {
     const restaurants = await this.restaurants.find({
       isPromoted: true,
